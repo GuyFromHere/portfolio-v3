14c2c43542e6a36f9d818611a31a941abac92c07
Dan Dismuke <dandismuke@gmail.com>
2020-03-24 00:24:34 -0700

    Fixed links and some style changes. Added in Contact page.

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,38 +7,32 @@ function NavBar() {
 		<div className="nav">
 			<div className="col-3 menu">
 				<ul>
-					<li>
-						<Link
-							to="/about"
-							className={
-								window.location.pathname === "/home" ? "nav-link active" : "nav-link"
-							}
-						>
-							About Me
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/portfolio"
-							className={
-								window.location.pathname === "/portfolio"
-									? "nav-link active"
-									: "nav-link"
-							}
-						>
-							Portfolio
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/contact"
-							className={
-								window.location.pathname === "/contact" ? "nav-link active" : "nav-link"
-							}
-						>
-							Contact
-						</Link>
-					</li>
+					<Link
+						to="/about"
+						className={
+							window.location.pathname === "/home" ? "nav-link active" : "nav-link"
+						}
+					>
+						<li>About Me</li>
+					</Link>
+					<Link
+						to="/portfolio"
+						className={
+							window.location.pathname === "/portfolio"
+								? "nav-link active"
+								: "nav-link"
+						}
+					>
+						<li>Portfolio</li>
+					</Link>
+					<Link
+						to="/contact"
+						className={
+							window.location.pathname === "/contact" ? "nav-link active" : "nav-link"
+						}
+					>
+						<li>Contact</li>
+					</Link>
 				</ul>
 			</div>
 		</div>
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,8 +1,40 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
+	email = "dandismuke@gmail.com";
+	github = "https://github.com/GuyFromHere";
+	linkedin = "https://www.linkedin.com/in/dan-dismuke-526a646b/";
+	phone = "503-953-2152";
+	resume = "https://drive.google.com/file/d/1pv6jqytz1OWVEwQXQORH0jp0jqUcB5Tk/view?usp=sharing";
+	
 	render() {
-		return <div class="container">Contact</div>;
+		return (
+			<div id="content" className="col-8">
+				<h1>Contact</h1>
+				<div id="contact">
+					<div className='email'>
+						<h4>Email:</h4>
+						{this.email}
+					</div>
+					<div className='phone'>
+						<h4>Phone:</h4>
+						{this.phone}
+					</div>
+					<div className='github'>
+						<h4>GitHub:</h4>
+						<a href={this.github} target='_blank' rel="noopener noreferrer">GuyFromHere</a>
+					</div>
+					<div className='linkedin'>
+						<h4>LinkedIn:</h4>
+						<a href={this.linkedin} target='_blank' rel="noopener noreferrer">LinkedIn</a>
+					</div>
+					<div className='resume'>
+						<h4>Resume:</h4>
+						<a href={this.resume} target='_blank' rel="noopener noreferrer">Download</a>
+					</div>
+				</div>	
+			</div>
+		);
 	}
 }
 
